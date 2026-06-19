687c252ac59e43d94a7b71f7973495f61158547f
fivetagz-prog <fivetagz@gmail.com>
2026-06-19 16:27:34 +0800

    Add MainActivity to load a web page in WebView

---
+++ MainActivity.java
@@ -0,0 +1,20 @@
+package com.izen.app;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://izen.lol");
+    }
+}
